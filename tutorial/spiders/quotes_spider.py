a58d2c81b55c36e16fcb5a6d0e2444fd6ae9e2c0
leixin <chjgreen@qq.com>
2020-08-11 17:25:57 +0800

    追踪链接(递归链接)

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -6,7 +6,6 @@ class QuotesSpider(scrapy.Spider):
 
     start_urls = [
             'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
         ]
 
     def parse(self, response):
@@ -16,3 +15,8 @@ class QuotesSpider(scrapy.Spider):
                 'author': quote.css('small.author::text').get(),
                 'tags': quote.css("div.tags a.tag::text").getall(),
             }
+
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
